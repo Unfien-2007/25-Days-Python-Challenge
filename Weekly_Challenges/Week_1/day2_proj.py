6786c0d7420191e8fec8981bd609cdcd2ee140a6
[Unfien-2007] <Unfienproductions@gmail.com>
2026-03-02 21:43:14 +0800

    day2_proj

---
+++ Weekly_Challenges/Week_1/day2_proj.py
@@ -0,0 +1,18 @@
+#salary calculator for the rone
+monthly_salary: float = float(input("Enter Monthly Salary: "))
+Yearly_salary: float = monthly_salary * 12
+_13month_salary: float = Yearly_salary / 12
+
+#
+
+
+
+
+
+#
+print("=" * 60)
+print("Monthly Salary Calculator")
+print("=" * 60)
+print(f"With PHP {monthly_salary:.2f} Monthly Salary")
+print(f"You would have PHP {Yearly_salary:.2f} Yearly Salary")
+print(f"and a total salary of PHP {Yearly_salary + _13month_salary:.2f} before tax.")
